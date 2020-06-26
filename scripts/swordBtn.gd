f16a355148ecf9849c57c53a698dee69a15894c3
Anthony Foxclaw <noreply@tonytins.xyz>
2020-06-26 11:57:03 -0400

    Slash effect

--- scripts/swordBtn.gd
+++ scripts/swordBtn.gd
@@ -1,10 +1,19 @@
 extends "res://scripts/baseBtn.gd"
 
+const slash = preload("res://scenes/slash.tscn")
+
 func _on_pressed():
 	var main = get_tree().current_scene
 	var enemy = main.find_node("enemy")
 	var stats = main.find_node("stats")
 	if enemy != null and stats != null:
+		create_slash(enemy.global_position)
 		enemy.take_damange(4)
 		stats.mp += 2
 		stats.ap -= 1
+
+func create_slash(pos):
+	var new_slash = slash.instance()
+	var main = get_tree().current_scene
+	main.add_child(new_slash)
+	new_slash.global_position = pos
